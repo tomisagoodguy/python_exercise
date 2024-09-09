c8d72e513f50b9a422ecf9ea4a7061c1dcfbef79
陳定康 <tom890108159@gmail.com>
2024-09-09 15:58:07 +0800

    commit

--- 變數.py
+++ 變數.py
@@ -1,3 +1,4 @@
+'''
 print("True+1",True+1)
 print(2.1+1)
 print(7+int('5'))
@@ -5,7 +6,7 @@ print(7+float("5"))
 print(str(7.0)+"5")
 print(int(7/3))
 print(bool(0))
-
+'''
 
 '''
 x=5
@@ -16,3 +17,13 @@ print(type(y))
 b=input('請輸入歲數')
 print("你預計",int(b)+5,"發大財")
 '''
+
+'''
+r= float(input("請輸入半徑:"))
+area = 3.141596*r**2  # 園面積
+perimeter = 2*3.141596*r  # 周長
+#print("園面積:%.2f\n周長:%.2f"%(area,perimeter))
+print(f"園面積:{area:.2f}\n周長:{perimeter:.2f}")
+
+'''
+
