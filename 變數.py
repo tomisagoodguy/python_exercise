c615b71e10569e66e7777a3ccfa3d83e7414f63e
陳定康 <tom890108159@gmail.com>
2024-09-09 11:39:22 +0800

    commit

---
+++ 變數.py
@@ -0,0 +1,18 @@
+print("True+1",True+1)
+print(2.1+1)
+print(7+int('5'))
+print(7+float("5"))
+print(str(7.0)+"5")
+print(int(7/3))
+print(bool(0))
+
+
+'''
+x=5
+y="john"
+print(type(x))
+print(type(y))
+# input() 函數返回的是字符串（string）類型。
+b=input('請輸入歲數')
+print("你預計",int(b)+5,"發大財")
+'''
